de3874d4ab0edce50a4178c1eedf033d63391cf5
Lukáš Vítek <Lukas.Vitek2@aricoma.com>
2025-01-20 09:45:20 +0100

    request extractor

--- src/requests/request-extractor.js
+++ src/requests/request-extractor.js
@@ -6,50 +6,38 @@ class RequestExtractor {
 
     extract(request, response) {
         const requests = [];
-        this.processChildren(request, response.data.item, requests);
-        
-        return requests;
-    }
-
-    processChildren(request, currentItem, requests) {
-        if (!currentItem || !currentItem.children || !currentItem.children.results) {
-            return;
-        }
-
-        for (const child of currentItem.children.results) {
-            const nameItem = child.name;
-            const path = `${request}/${nameItem}`;
-            requests.push({
-                url: this.apiUrl,
-                method: 'POST',
-                headers: {
-                    'content-type': ['application/json'],
-                    'sc_apikey': [this.apiKey],
-                },
-                body: JSON.stringify({
-                    query: `query getItem($path: String) {
-                        item(language: "en", path: $path) {
-                            id
-                            path
-                            rendered
-                            children(first: 100) {
-                                results {
-                                    name
-                                    rendered
+        if (response && response.data && response.data.item && response.data.item.children) {
+            requests = response.body.data.item.children.results.map((e, i) => {
+                const path = `${request}/${child.name}`;
+                requests.push({
+                    url: this.apiUrl,
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json',
+                        'sc_apikey': this.apiKey,
+                    },
+                    body: JSON.stringify({
+                        query: `query getItem($path: String) {
+                            item(language: "en", path: $path) {
+                                id
+                                path
+                                rendered
+                                children(first: 100) {
+                                    results {
+                                        name
+                                        rendered
+                                    }
                                 }
                             }
-                        }
-                    }`,
-                    operationName: 'getItem',
-                    variables: { path },
-                }),
+                        }`,
+                        operationName: 'getItem',
+                        variables: { path },
+                    }),
+                });
             });
-
-            // Rekurzivně zpracuj podpotomky
-            if (child.children) {
-                this.processChildren(path, child, requests);
-            }
         }
+
+        return requests;
     }
 }
 
